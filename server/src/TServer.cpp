ef0ff30434e64d97402a057d7ee7903a9208d2fd
marlon@eevul.org <marlon@eevul.org@7aab1f78-3e5b-11de-917f-4137a9cc17b9>
2012-12-05 17:00:53 +0000

    Serverlist-v2: * Another potential fix for global PMs.

--- server/src/TServer.cpp
+++ server/src/TServer.cpp
@@ -1098,6 +1098,7 @@ bool TServer::msgSVI_PMPLAYER(CString& pPacket)
 			serverlog.out(CString() << "Sending PM (" << message << ") to " << account2 << " on " << servername << "\n");
 			// Send the pm to the appropriate server.
 			server->sendPacket(CString() >> (char)SVO_PMPLAYER << CString(name << "\n" << account << "\n" << nick << "\n" << weapon << "\n" << type << "\n" << account2 << "\n" << message).gtokenizeI() << "\n");
+			break;
 		}
 	}
 
