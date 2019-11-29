c84c270a27c30134c824a3a9bae588b46b9018da
joey <1166538+xtjoeytx@users.noreply.github.com>
2019-11-28 21:06:55 -0500

    Potential fix for RC -> IRC

--- server/src/ServerConnection.cpp
+++ server/src/ServerConnection.cpp
@@ -1326,6 +1326,15 @@ bool ServerConnection::msgSVI_REQUESTLIST(CString& pPacket)
 							ircServer->removePlayerFromChannel(channel, player->getIrcStub());
 						}
 					}
+					else if (params.size() == 6 && params[2] == "privmsg")
+					{
+						std::string from = params[3].text();
+						std::string channel = params[4].text();
+						std::string message = params[5].text();
+
+						ServerPlayer *player = getPlayer(from);
+						_listServer->getIrcServer()->sendTextToChannel(channel, message, player->getIrcStub());
+					}
 				}
 			}
 		}
